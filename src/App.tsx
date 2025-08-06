0a6c9adf0cd88bb97624024bc15db83af8063de0
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-06 15:26:22 -0300

    feat: add logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { Heading } from './components/Heading';
-import { Container } from './components/Container';
-
 import './styles/global.css';
 import './styles/theme.css';
+
+import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
 
       <Container>
