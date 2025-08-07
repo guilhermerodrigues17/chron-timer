b765cc8ddcc6d8ced91872f0d306b7e31e3ce2fe
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-07 16:29:17 -0300

    feat: add theme change logic

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,15 +1,16 @@
+import type React from 'react';
 import styles from './styles.module.css';
 
 type LinkProps = {
   href: string;
   children: React.ReactNode;
   className?: string;
-};
+} & React.ComponentProps<'a'>;
 
-export function Link({ href, children, className }: LinkProps) {
+export function Link({ href, children, className, ...props }: LinkProps) {
   const styleClassName = className ? className : styles.link;
   return (
-    <a href={href} className={styleClassName}>
+    <a href={href} className={styleClassName} {...props}>
       {children}
     </a>
   );
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,9 +1,44 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 
 import styles from './styles.module.css';
 import { Link } from '../Link';
+import { useEffect, useState } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const savedTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return savedTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const updatedTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return updatedTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
       <Link href='#'>
@@ -18,8 +53,8 @@ export function Menu() {
         <SettingsIcon />
       </Link>
 
-      <Link href='#'>
-        <SunIcon />
+      <Link href='#' onClick={handleThemeChange}>
+        {nextThemeIcon[theme]}
       </Link>
     </nav>
   );
