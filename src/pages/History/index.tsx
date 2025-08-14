e83edb7a510cb0718e4213985eceea90eac77a9d
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-14 18:35:18 -0300

    feat: add history page logic

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,67 @@
+import styles from './styles.module.css';
+
+import { Trash } from 'lucide-react';
+import { Container } from '../../components/Container';
+import { DefaultButton } from '../../components/DefaultButton';
+import { Heading } from '../../components/Heading';
+import { MainTemplate } from '../../templates/MainTemplate';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
+
+export function History() {
+  const { state } = useTaskContext();
+
+  const taskTypeMap = {
+    workTime: 'Foco',
+    shortBreak: 'Descanso curto',
+    longBreak: 'Descanso longo',
+  };
+
+  return (
+    <MainTemplate>
+      <Container>
+        <Heading>
+          <span>Histórico</span>
+          <span className={styles.buttonContainer}>
+            <DefaultButton
+              icon={<Trash />}
+              color='red'
+              aria-label='Apagar todo o histórico'
+              title='Apagar histórico'
+            />
+          </span>
+        </Heading>
+      </Container>
+
+      <Container>
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.tasks.map(task => {
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeMap[task.type]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Container>
+    </MainTemplate>
+  );
+}
